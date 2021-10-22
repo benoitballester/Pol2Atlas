e48027b0b93ddcfe27e96fd46aa79e4b13e0d8d5
Delangen <pdelangen@smp005.cluster>
2021-10-22 10:36:59 +0200

    added great

--- lib/utils/overlap_utils.py
+++ lib/utils/overlap_utils.py
@@ -1,29 +1,37 @@
 import numpy as np
 import pandas as pd
 import pyranges as pr
-import numba as nb
 from scipy.stats import hypergeom
 from statsmodels.stats.multitest import fdrcorrection
+from rpy2.robjects.packages import importr
+from rpy2.robjects import numpy2ri
+numpy2ri.activate()
+rs = importr("stats")
 
-def dfToPrWorkaround(df):
+def dfToPrWorkaround(df, useSummit=True):
     # There is an issue when converting a dataframe to a pyrange
     # Convert to separate np array beforehand
-    return pr.PyRanges(chromosomes=df.iloc[:,0].values.ravel(), 
-                        starts=df.iloc[:,1].values.ravel(), 
-                        ends=df.iloc[:,2].values.ravel())
+    if useSummit:
+        return pr.PyRanges(chromosomes=df.iloc[:,0].values.ravel(), 
+                            starts=df.iloc[:,6].values.ravel(), 
+                            ends=df.iloc[:,7].values.ravel())
+    else:
+        return pr.PyRanges(chromosomes=df.iloc[:,0].values.ravel(), 
+                            starts=df.iloc[:,1].values.ravel(), 
+                            ends=df.iloc[:,2].values.ravel())
 
 def getIntersected(query, catalog):
     pass
 
 def countOverlapPerCategory(catalog, query):
-    return catalog.intersect(query).as_df()["Name"].value_counts().to_dict()
+    return catalog.overlap(query, how=None).as_df()["Name"].value_counts()
 
 
 def countOverlaps(catalog, query):
-    return len(catalog.intersect(query))
+    return len(catalog.overlap(query))
 
 
-def computeEnrichVsBg(catalog, query, universe):
+def computeEnrichVsBg(catalog, universe, query):
     """
     Computes an hypergeometric enrichment test on the number of intersections
     for different classes of genomic elements (catalog).
@@ -34,12 +42,47 @@ def computeEnrichVsBg(catalog, query, universe):
         Elements to find enrichment on.
         PyRanges having the category of the genomic element in the "name" column.
     query: PyRanges
-        The genomic regions on interest. Must be contained in universe.
+        The genomic regions on interest. Must be contained in universe for correct
+        results.
     universe: PyRanges
-        The background regions whose intersection count serves as the null
-        distribution.
+        The background regions whose intersection count serves as the expected results.
+    
+    Returns
+    -------
+    pvalues: pandas Series
+        pvalues for each class of genomic element
+    fc: pandas Series
+        Fold change for each lass of genomic element
+    qvalues: pandas Series
+        Benjamini-Hochberg qvalues for each class of genomic element
     """
-    pass
+    # Compute intersections for universe
+    refCounts = countOverlapPerCategory(catalog, dfToPrWorkaround(universe))
+    allCats = np.array(list(refCounts.keys()))
+    pvals = np.zeros(len(allCats))
+    fc = np.zeros(len(allCats))
+    M = len(universe)
+    # Then for the query
+    obsCounts = countOverlapPerCategory(catalog, dfToPrWorkaround(query))
+    N = len(query)
+    # Find hypergeometric enrichment
+    k = pd.Series(np.zeros(len(allCats), dtype="int"), allCats)
+    isFound = np.isin(allCats, obsCounts.index , assume_unique=True)
+    k[allCats[isFound]] = obsCounts
+    n = pd.Series(np.zeros(len(allCats), dtype="int"), allCats)
+    n[allCats] = refCounts
+    pvals = np.array(rs.phyper(k.values-1,n.values,M-n.values,N, lower_tail=False))
+    pvals = np.nan_to_num(pvals, nan=1.0)
+    fc = (k/np.maximum(N, 1e-7))/np.maximum(n/np.maximum(M, 1e-7), 1e-7)
+    qvals = fdrcorrection(pvals)[1]
+    pvals = pd.Series(pvals)
+    pvals.index = allCats
+    qvals = pd.Series(qvals)
+    qvals.index = allCats
+    fc = pd.Series(fc)
+    fc.index = allCats
+    return pvals, fc, qvals
+
 
 def computeEnrichForLabels(catalog, universe, labels):
     """
@@ -69,7 +112,7 @@ def computeEnrichForLabels(catalog, universe, labels):
         
     """
     allLabels = np.unique(labels)
-    refCounts = countOverlapPerCategory(catalog, universe)
+    refCounts = countOverlapPerCategory(catalog, dfToPrWorkaround(universe))
     allCats = np.array(list(refCounts.keys()))
     pvals = np.zeros((len(allLabels), len(allCats)))
     fc = np.zeros((len(allLabels), len(allCats)))
@@ -77,7 +120,7 @@ def computeEnrichForLabels(catalog, universe, labels):
     i = 0
     for l in allLabels:
         query = universe[labels == l]
-        obsCounts = countOverlapPerCategory(catalog, query)
+        obsCounts = countOverlapPerCategory(catalog, dfToPrWorkaround(query))
         N = len(query)
         j = 0
         for c in allCats:
@@ -104,16 +147,3 @@ def computeEnrichForLabels(catalog, universe, labels):
     
 
     
-
-@nb.njit(parallel=True)
-def addGenomicContext(starts, ends, annots, array):
-    for i in nb.prange(len(starts)):
-        array[starts[i]:ends[i]] = np.maximum(array[starts[i]:ends[i]], annots[i])
-
-
-@nb.njit(parallel=True)
-def findPrioritaryContext(starts, ends, array):
-    contextes = np.zeros(len(starts), array.dtype)
-    for i in nb.prange(len(starts)):
-        contextes[i] = np.maximum(array[starts[i]:ends[i]])
-    return contextes
