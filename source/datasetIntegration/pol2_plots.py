5b9e034db84004c354a1e21985bff4cb6bb85ce2
Pierre de Langen <pdelangen13@cpu-node-55.ifb.local>
2022-10-21 15:28:21 +0200

    Fixed plots for editing, retrieved a missing script

---
+++ source/datasetIntegration/pol2_plots.py
@@ -0,0 +1,53 @@
+# %%
+# Pol II only, Run this after the integrative analysis
+import sys
+
+import numpy as np
+from lib.peakMerge import peakMerger
+from lib.utils import matrix_utils, overlap_utils, plot_utils
+
+sys.path.append("./")
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import pyranges as pr
+import seaborn as sns
+from settings import params, paths
+from scipy.io import mmread
+donutSize = 0.3
+target = "POLR2A"
+listFiles = os.listdir(paths.peaksFolder)
+outputDir = paths.outputDir + "descriptivePlots/"
+try:
+    os.mkdir(outputDir)
+except FileExistsError:
+    pass
+# %%
+isEncode = [1 if i.startswith("ENCS") else 0 for i in listFiles]
+counts = np.bincount(isEncode)
+labels = ["GEO", "ENCODE"]
+palette = plot_utils.getPalette(isEncode)[0]
+plot_utils.donutPlot(donutSize, counts, counts.sum(), labels, 
+          counts.sum(), target + " ChIP-seq", palette, fName=outputDir + "expOrigin", showPct=False)
+
+# %%
+annotation = pd.read_csv(paths.annotationFile, sep="\t", index_col="Sample")
+fileAnnot, annots = pd.factorize(annotation.loc[listFiles]["Annotation"])
+counts = np.bincount(fileAnnot)
+palette = plot_utils.getPalette(fileAnnot)[0]
+plot_utils.donutPlot(donutSize, counts, counts.sum(), annots, 
+          counts.sum(), target + " ChIP-seq", palette, fName=outputDir + "expBiotype", showPct=False)
+# %%
+plt.figure(dpi=300, figsize=(3,2))
+mat = mmread(paths.outputDir + "matrix.mtx")
+distribPeaks = np.array(mat.sum(axis=1)).ravel()
+maxVal = int(np.percentile(distribPeaks,95))
+n, bins, _ = plt.hist(np.clip(distribPeaks, 0, maxVal), np.arange(0, 1+maxVal))
+plt.text(bins[-2]*0.5+bins[-1]*0.5, n[-1] + 0.05*plt.ylim()[1], 
+         str(maxVal) + "+", ha="center", size=8)
+plt.xlabel("Peak observed in x experiments", size=8)
+plt.ylabel("Number of peaks", size=8)
+plt.tick_params(labelsize=8)
+plt.savefig(paths.outputDir + "descriptivePlots/distrib_overlap_per_peak.pdf", bbox_inches="tight")
+plt.show()
