2d3dd0fe9ae09ea9fe42fafc505df32e990f32e0
Pierre de Langen <pierre.de-langen@univ-amu.fr>
2023-03-20 16:51:24 +0100

    .

---
+++ settings_set/all_Pol2/paths.py
@@ -0,0 +1,29 @@
+# ---------- Common ---------- 
+outputDir = "/scratch/pdelangen/projet_these/outputPol2_All/"   # Main results
+tempDir = "/scratch/pdelangen/projet_these/tempPol2_All/"       # Mostly temporary/unimportant
+genomeFile = "/scratch/pdelangen/projet_these/data_clean/hg38.chrom.sizes.sorted"
+annotationFile = "/scratch/pdelangen/projet_these/data_clean/annotPol2_All.tsv"
+
+# ---------- Required for peak integrative analysis ---------- 
+peaksFolder = "/scratch/pdelangen/projet_these/data_clean/peaks/"
+
+# ---------- Pol II specific ---------- 
+# You will likely not need this for your analyses
+gencode = "/scratch/pdelangen/projet_these/data/annotation/gencode.v38.annotation.gtf"
+bedtoolsPath = "/home/pdelangen/bedtools"
+encodeBlacklist = "/scratch/pdelangen/projet_these/data_clean/hg38-blacklist.v2.bed"
+# ---------- Required for GWAS analysis ---------- 
+
+
+# ---------- Required to download counts from TCGA ---------- 
+tokenPath = "/scratch/pdelangen/projet_these/data/tcga/token.txt"
+manifest = "/scratch/pdelangen/projet_these/data_clean/gdc_manifest.2021-09-22.txt"
+gdcClientPath = "/scratch/pdelangen/projet_these/data/tcga/gdc-client"
+
+# ---------- Required only to read counts ---------- 
+featureCountPath = "/scratch/pdelangen/projet_these/data/tcga/featureCounts"
+
+# ---------- Required for RNA-seq analysis ---------- 
+countDirectory = "/scratch/pdelangen/projet_these/output_POL2_lenient_largerTSS_latestGENCODE/counts/500centroid/"
+atlasPath = "/scratch/pdelangen/projet_these/temp_POL2_lenient_largerTSS_latestGENCODE/POLR2A_Inter_500.saf"
+tableDirectory = outputDir + "rnaseq"
---
+++ settings_set/interg_pol2/params copy.py
@@ -0,0 +1,7 @@
+import matplotlib as mpl
+mpl.rcParams['pdf.fonttype'] = 42
+inferCenter=False
+fileFormat="bed"
+scoreMethod="binary"
+minOverlap = 2
+removeGeneTail5kb = False
